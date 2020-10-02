45569e1774d3bca9eeed9331aed754cf1a37ed2a
charul97 <charulagrawal97@gmail.com>
2020-10-02 13:36:15 +0530

    Update s3BucketAccessAutomationRemediation.py

--- s3BucketAccessAutomationRemediation.py
+++ s3BucketAccessAutomationRemediation.py
@@ -1,1 +1,14 @@
 # This code remediates S3 bucket with public access ON and turns it OFF
+def handler(event, context):
+
+    #parse event
+    reqParams = event['detail']['requestParameters']
+    response=client.put_public_access_block(
+                    Bucket=bucketName,
+                    PublicAccessBlockConfiguration={
+                      'BlockPublicAcls': True,
+                      'IgnorePublicAcls': True,
+                      'BlockPublicPolicy': True,
+                      'RestrictPublicBuckets': True
+                  },
+                )
