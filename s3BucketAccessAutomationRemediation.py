4f93f1abb0e175a29115bced0d9a38bf517a2578
Spar1O <72181038+Spar1O@users.noreply.github.com>
2020-10-02 14:23:25 +0530

    Added line 8
    "client" was missing and was referenced in line 9.

--- s3BucketAccessAutomationRemediation.py
+++ s3BucketAccessAutomationRemediation.py
@@ -1,10 +1,11 @@
 # This code remediates S3 bucket with public access ON and turns it OFF
 import boto3
 def handler(event, context):
-
+    
     #parse event
     reqParams = event['detail']['requestParameters']
     bucketName= reqParams['bucketName']
+    client=boto3.client('s3')                    #added
     response=client.put_public_access_block(
                     Bucket=bucketName,
                     PublicAccessBlockConfiguration={
