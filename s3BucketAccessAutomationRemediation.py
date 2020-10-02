4569141769f79886436c4953b7b7d0a6d6cbacbc
Shreshth17 <71917978+Shreshth17@users.noreply.github.com>
2020-10-02 14:20:30 +0530

    Added line 2,7
    Added library boto3 and a variable "bucketName" (it was referenced but not assigned)

--- s3BucketAccessAutomationRemediation.py
+++ s3BucketAccessAutomationRemediation.py
@@ -1,8 +1,10 @@
 # This code remediates S3 bucket with public access ON and turns it OFF
+import boto3
 def handler(event, context):
 
     #parse event
     reqParams = event['detail']['requestParameters']
+    bucketName= reqParams['bucketName']
     response=client.put_public_access_block(
                     Bucket=bucketName,
                     PublicAccessBlockConfiguration={
