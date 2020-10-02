d2fd5aef73d48c5ef77602fd0defbdf36a50a37c
Arun Yadav <aruner02@gmail.com>
2020-10-02 13:23:44 +0530

    Added new line (7)
    variable "bucketName" was referenced before assignment

--- s3EncryptionAutomationRemediation.py
+++ s3EncryptionAutomationRemediation.py
@@ -4,6 +4,7 @@ def handler(event, context):
 
     #parse event
     reqParams = event['detail']['requestParameters']
+    bucketName = reqParams['bucketName']    # added bucketname 
     response = s3Client.put_bucket_encryption(
             Bucket=bucketName,
             ServerSideEncryptionConfiguration={
